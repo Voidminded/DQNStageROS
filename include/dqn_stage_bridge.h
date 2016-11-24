c6f238d7c3d818e7933459678042de7125d3d92c
Voidminded <voidminded@gmail.com>
2016-11-24 11:51:58 -0800

    Publishing lase data as image

--- include/dqn_stage_bridge.h
+++ include/dqn_stage_bridge.h
@@ -2,6 +2,17 @@
 #define DQN_STAGE_BRIDGE_H
 
 #include "ros/ros.h"
+#include "sensor_msgs/LaserScan.h"
+#include "nav_msgs/Odometry.h"
+#include "tf/tf.h"
+#include "image_transport/image_transport.h"
+#include "cv_bridge/cv_bridge.h"
+#include "sensor_msgs/image_encodings.h"
+#include <opencv2/opencv.hpp>
+
+#define PI 3.14159265359
+
+using namespace cv;
 
 class DQNStageBridge{
 public:
@@ -10,7 +21,18 @@ public:
 protected:
   ros::NodeHandle nh_;
 private:
-	virtual void SpinOnce();
+  Mat laserMap;
+  geometry_msgs::Pose robotPose;
+
+  ros::Subscriber laser_sub_;
+  ros::Subscriber odom_sub_;
+  image_transport::ImageTransport it;
+  image_transport::Publisher pub_laser_iamge_;
+
+  virtual void SpinOnce();
+
+  void generateOccupancyGridCallBack(const  sensor_msgs::LaserScanConstPtr& scan);
+  void updateRobotPoseCallBack(const  nav_msgs::OdometryConstPtr& odom);
 };
 
 #endif 
--- src/dqn_stage_bridge.cpp
+++ src/dqn_stage_bridge.cpp
@@ -1,9 +1,44 @@
 #include "dqn_stage_bridge.h"
+//#include "include/dqn_stage_bridge.h"
 
 DQNStageBridge::DQNStageBridge(ros::NodeHandle& nh)
-  :nh_(nh)
+  :nh_(nh),
+    it(nh_),
+    laser_sub_(nh_.subscribe("base_scan", 30, &DQNStageBridge::generateOccupancyGridCallBack, this)),
+    odom_sub_(nh_.subscribe("base_pose_ground_truth", 30, &DQNStageBridge::updateRobotPoseCallBack, this))
 {
+  pub_laser_iamge_ = it.advertise("laser_image", 1);
+  laserMap.create( 120, 120, CV_8U);
+}
 
+void DQNStageBridge::generateOccupancyGridCallBack(const sensor_msgs::LaserScanConstPtr &scan)
+{
+  if( scan->ranges.size() < 1)
+    return;
+  else
+    {
+      laserMap = Scalar( 128, 128, 128);
+      int x = scan->range_max* 10, y = scan->range_max*10, rad = scan->range_max*10;
+      tf::Pose p;
+      tf::poseMsgToTF(robotPose, p);
+      int dir = 180*tf::getYaw(p.getRotation())/PI;
+      for( float ind = 0; ind < 360; ind++)
+        {
+          int i = ind + dir;
+          for( int j = 0; j < rad; j++)
+            if( scan->ranges.at(ind)*10>=j)
+              laserMap.data[ (int)scan->range_max *20 *(x+ int( j*cos( PI*i/180))) + y + int( j*sin( PI*i/180))] = 255;
+          if( scan->ranges.at(ind) < 6)
+            laserMap.data[ (int)scan->range_max*20 *(x+ int( 10*scan->ranges.at(ind)*cos( PI*i/180))) + y + int( 10*scan->ranges.at(ind)*sin( PI*i/180))] = 0;
+        }
+      sensor_msgs::ImagePtr laserImageMSG = cv_bridge::CvImage(std_msgs::Header(), "mono8", laserMap).toImageMsg();
+      pub_laser_iamge_.publish( laserImageMSG);
+    }
+}
+
+void DQNStageBridge::updateRobotPoseCallBack(const nav_msgs::OdometryConstPtr &odom)
+{
+  robotPose = odom->pose.pose;
 }
 
 void DQNStageBridge::SpinOnce()
@@ -15,15 +50,15 @@ void DQNStageBridge::Spin()
 {
   //ros::Rate rate(update_rate_);
   while (ros::ok())
-  {
-    SpinOnce();
-    // TODO: Graceful shutdown:
-    // http://answers.ros.org/question/27655/what-is-the-correct-way-to-do-stuff-before-a-node-is-shutdown/
-    // if (!rate.sleep())
-    // {
-    //   ROS_WARN_STREAM("[ANIM] missed target loop rate of " << update_rate_ << " hz.");
-    // }
-  }
+    {
+      SpinOnce();
+      // TODO: Graceful shutdown:
+      // http://answers.ros.org/question/27655/what-is-the-correct-way-to-do-stuff-before-a-node-is-shutdown/
+      // if (!rate.sleep())
+      // {
+      //   ROS_WARN_STREAM("[ANIM] missed target loop rate of " << update_rate_ << " hz.");
+      // }
+    }
 
 }
 
