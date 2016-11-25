bc1404d1d19e9a525010aa15f5cb816dc0c8a705
Voidminded <voidminded@gmail.com>
2016-11-24 20:18:12 -0800

    DAMN ! Creating environment is hard

--- script/dqn_node.py
+++ script/dqn_node.py
@@ -1,10 +1,55 @@
 #!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import Image
+from std_srvs import Empty
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
+import random
+import logging
+import numpy as np
 
 bridge = CvBridge()
+
+logger = logging.getLogger(__name__)
+
+class StageEnvironment(object):
+  def __init__(self, max_random_start,
+               observation_dims, data_format):
+    self.env = gym.make(env_name)
+
+    self.max_random_start = max_random_start
+    self.action_size = 27
+
+    self.data_format = data_format
+    self.observation_dims = observation_dims
+
+    self.screen = np.zeros((observation_dims,observation_dims,1), np.uint8)
+    rospy.wait_for_service('reset_positions')
+    self.resetStage = rospy.ServiceProxy('reset_positions', Empty)
+  
+
+  def new_game(self):
+    self.resetStage()
+    return self.preprocess(), 0, False
+
+  def new_random_game(self):
+    return self.new_game()
+
+  def step(self, action, is_training=False):
+    observation, reward, terminal, info = self.env.step(action)
+    if self.display: self.env.render()
+    return self.preprocess(observation), reward, terminal, info
+
+  def preprocess(self):
+    return self.screen
+
+  def laserCB(self, data):
+    try:
+      self.screen = bridge.imgmsg_to_cv2(data, "mono8")
+    except CvBridgeError as e:
+      print(e)
+    cv2.imshow("Scree", screen)
+    cv2.waitKey(3)
 
 # Create publisher
 #publisher = rospy.Publisher("~topic",String,queue_size=1)
@@ -21,14 +66,6 @@ pub_period = rospy.get_param("~pub_period",10.0)
 
 # Create timer
 #rospy.Timer(rospy.Duration.from_sec(pub_period),callback)
-
-def laserCB(data):
-  try:
-    screen = bridge.imgmsg_to_cv2(data, "mono8")
-  except CvBridgeError as e:
-    print(e)
-  cv2.imshow("Scree", screen)
-  cv2.waitKey(3)
 
 if __name__ == '__main__':
   # Initialize the node with rospy
