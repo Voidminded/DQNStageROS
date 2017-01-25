f0c822e7b8bfecc37d336519204a71cdd99eed54
Sepehr MohaimenianPour <voidminded@gmail.com>
2017-01-24 18:40:17 -0800

    writing robot controller

--- src/robot_ctrl.cpp
+++ src/robot_ctrl.cpp
@@ -1,43 +1,95 @@
 #include <stage.hh>
 #include "ros/ros.h"
 #include <geometry_msgs/PoseStamped.h>
+#include <sensor_msgs/LaserScan.h>
+#include <dqn_stage_ros/stage_message.h>
 
 using namespace Stg;
-Model* target;
+
+struct ModelRobot
+{
+    ModelPosition* pos;
+    ModelRanger* laser;
+};
+
+ModelRobot* robot;
+
 ros::NodeHandle* n;
-ros::Subscriber sub;
+ros::Publisher pub_state_;
 
-void updateCB(const geometry_msgs::PoseStampedConstPtr &pose)
+geometry_msgs::Pose rosCurPose;
+sensor_msgs::LaserScan rosLaserData;
+bool collision = false;
+double minFrontDist;
+
+void stgPoseUpdateCB( Model* mod, ModelRobot* robot)
 {
-    ROS_WARN("ROS Sub");
-    Pose p;
-    p.x = pose->pose.position.x;
-    p.y = pose->pose.position.y;
-    p.z = 0;
-    target->SetPose( p);
-    return;
+    geometry_msgs::PoseStamped positionMsg;
+    positionMsg.pose.position.x = robot->pos->pose.x;
+    positionMsg.pose.position.y = robot->pos->pose.y;
+    positionMsg.pose.position.z = robot->pos->pose.z;
+//    positionMsg.pose.orientation
+    positionMsg.header.stamp = ros::Time::now();
+    rosCurPose = positionMsg;
 }
 
-int spinCB( Model* mod, ros::NodeHandle* dummy)
+void stgLaserCB( Model* mod, ModelRobot* robot)
 {
-    ros::spinOnce();
-    return 0;
-}
+    sensor_msgs::LaserScan laserMsgs;
+    const Stg::ModelRanger::Sensor& sensor = robot->laser->GetSensors()[0];
+    if( sensor.ranges.size() )
+    {
+        // Translate into ROS message format and publish
+        laserMsgs.angle_min = -sensor.fov/2.0;
+        laserMsgs.angle_max = +sensor.fov/2.0;
+        laserMsgs.angle_increment = sensor.fov/(double)(sensor.sample_count-1);
+        laserMsgs.range_min = sensor.range.min;
+        laserMsgs.range_max = sensor.range.max;
+        laserMsgs.ranges.resize(sensor.ranges.size());
+        laserMsgs.intensities.resize(sensor.intensities.size());
 
+        collision = false;
+        minFrontDist = sensor.range.max;
+        // added by sepehr for random position init:
+//        double min_laser_val = 99;
+        for(unsigned int i = 0; i < sensor.ranges.size(); i++)
+        {
+            laserMsgs.ranges[i] = sensor.ranges[i];
+            if(sensor.ranges[i] < 0.3)
+                collision = true;
+            if( i > (sensor.fov - 45)/2 && i < (sensor.fov + 45)/2 && sensor.ranges[i]  < minFrontDist)
+                minFrontDist = sensor.ranges[i];
+//            if(sensor.ranges[i] < min_laser_val)
+//                min_laser_val = sensor.ranges[i];
+            laserMsgs.intensities[i] = sensor.intensities[i];
+        }
+
+//        if( min_laser_val > 3.3 && rand()/float(RAND_MAX) > 0.1)
+//        {
+//            initial_poses.clear();
+//            initial_poses.push_back(robotmodel->positionmodel->GetGlobalPose());
+//        }
+        laserMsgs.header.stamp = ros::Time::now();
+        rosLaserData = laserMsgs;
+    }
+}
 
 extern "C" int Init( Model* mod )
 { 
-	int argc = 0;
-	char** argv; 
-	ros::init( argc, argv, "target_controller_node");
+    int argc = 0;
+    char** argv;
+    ros::init( argc, argv, "target_controller_node");
     n = new ros::NodeHandle();
-    sub = n->subscribe("/target", 15, &updateCB);
-    target = /*(ModelPosition* )*/mod;
+    pub_state_ = n->advertise<dqn_stage_ros::stage_message>("input_data", 15);
 
-  // attach callback to be called whenever a flag is added to the sink model 
-    target->AddCallback( Model::CB_UPDATE, (model_callback_t)spinCB, NULL );
-    target->Subscribe();
-//    ros::spin();
+    robot = new ModelRobot;
+    robot->pos = (ModelPosition*) mod;
+    robot->pos->AddCallback( Model::CB_UPDATE, (model_callback_t)stgPoseUpdateCB, robot);
+    robot->pos->Subscribe();
+    //    robot->pos->GetChild("ranger:0")->Subscribe();
+    robot->laser = (ModelRanger*)mod->GetChild("ranger:0");
+    robot->laser->AddCallback( Model::CB_UPDATE, (model_callback_t)stgLaserCB, robot);
+    robot->laser->Subscribe();
     return 0; //ok
 }
 
